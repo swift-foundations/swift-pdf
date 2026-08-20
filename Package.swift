fb22402c549dc8849c5804825b5f167541c7cd12
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:02:01 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -26,11 +26,11 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-pdf",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(name: .pdf, targets: [.pdf]),
@@ -38,7 +38,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-foundations/swift-html.git", branch: "main"),
-        .package(url: "https://github.com/swift-foundations/swift-pdf-html-render.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-foundations/swift-pdf-html-render.git",
+            branch: "main"
+        ),
         .package(url: "https://github.com/swift-foundations/swift-file-system.git", branch: "main"),
     ],
     targets: [
@@ -53,7 +56,7 @@ let package = Package(
         .target(
             name: "PDF Test Support",
             dependencies: [
-                .pdf,
+                .pdf
             ],
             path: "Tests/Support"
         ),
